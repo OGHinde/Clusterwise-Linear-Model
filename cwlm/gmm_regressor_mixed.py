01e84ef9d65d09b02e6fad4cbe3982c8f44858a5
Oscar Hinde <oghinde@gmail.com>
2019-09-10 12:54:02 +0200

    mixed KM and GMM implemented (they need testing tho)

--- cwlm/gmm_regressor_mixed.py
+++ cwlm/gmm_regressor_mixed.py
@@ -118,68 +118,71 @@ class GMMRegressor(object):
         self.verbose = verbose
         self.random_state = random_state
         
-    def _check_data(self, X, y):
+    def _check_data(self, X_gmm, X_reg, y):
         """Check that the input data is correctly formatted.
 
         Parameters
         ----------
-        X : array-like, shape (n_samples, n_features)
-
+        X_gmm : array-like, shape (n_samples, n_gmm_features)
+        X_reg : array-like, shape (n_samples, n_reg_features)
         y : array, shape (n_samples, n_targets)
 
         Returns
         -------
-        t : int
-            The total number of targets.
-        
-        n : int
-            The total number of samples.
-
-        d : int
-            The total number of features (dimensions)
-
+        t : int, the total number of targets
+        n : int, the total number of samples
+        d_gmm : int, the total number of GMM features
+        d_reg : int, the total number of regression features
         """
 
         if y.ndim == 1:
             y = y[:, np.newaxis]
 
-        n_x, d = X.shape
+        n_x_gmm, d_gmm = X_gmm.shape
+        n_x_reg, d_reg = X_reg.shape
         n_y, t = y.shape
+
+        if n_x_gmm == n_x_gmm:
+            n_x = n_x_gmm
+        else:
+            error_report = 'Both views of the input data must have 1 to 1 sample correspondence.'
+            error_details = '\nX_gmm has {} samples while X_reg has {} samples.'.format(n_x_gmm, n_x_reg) 
+            raise ValueError(error_report, error_details)
 
         if n_x == n_y:
             n = n_x
         else:
-            print('Data size error. Number of samples in X and y must match:')
-            print('X n_samples = {}, y n_samples = {}'.format(n_x, n_y))
-            print('Exiting.')
-            sys.exit()
+            error_report = 'Data size error. Number of samples in X and y must match:'
+            error_details = 'X n_samples = {}, y n_samples = {}'.format(n_x, n_y)
+            raise ValueError(error_report, error_details)
 
-        return t, n, d, X, y
+        return t, n, d_gmm, d_reg, X_gmm, X_reg, y
 
-    def fit(self, X, y):
+    def fit(self, X_gmm, X_reg y):
         self.is_fitted_ = False
-        t, n, d, X, y = self._check_data(X, y)
+        t, n, d_gmm, d_reg, X_gmm, X_reg, y = self._check_data(X, y)
         eps = 10 * np.finfo(float).eps
         
         # Determine training sample/component posterior probability
         gmm = GMM(n_components=self.n_components, 
                   n_init=self.n_init, 
                   random_state=self.random_state)
-        gmm.fit(X)
-        resp_tr = gmm.predict_proba(X)
+        gmm.fit(X_gmm)
+        resp_tr = gmm.predict_proba(X_gmm)
         labels_tr = np.argmax(resp_tr, axis=1)
         
         # Calculate regression weights & precisions conditioned on 
         # posterior probabilities
-        reg_weights = np.empty((t, d+1, self.n_components))
+        reg_weights = np.empty((t, d_reg + 1, self.n_components))
         reg_precisions = np.zeros((t, self.n_components))
         for k in range(self.n_components):
             (reg_weights[:, :, k], 
-            reg_precisions[:, k]) = _estimate_regression_params_k(X, y, 
+            reg_precisions[:, k]) = _estimate_regression_params_k(X_reg, y, 
                 resp_k=resp_tr[:, k], alpha=self.alpha, weight_k=gmm.weights_[k])
         
         self.n_tasks_ = t
-        self.n_input_dims_ = d
+        self.n_gmm_dims_ = d_gmm
+        self.n_reg_dims_ = d_reg
         self.resp_tr_ = resp_tr
         self.labels_tr_ = labels_tr
         self.reg_weights_ = reg_weights.squeeze()
@@ -187,15 +190,16 @@ class GMMRegressor(object):
         self.gmm_ = gmm
         self.is_fitted_ = True
     
-    def predict(self, X):
-        if not self.is_fitted_: 
-            print("Model isn't fitted.")
-            return
+    def predict(self, X_gmm, X_reg):
+        if not self.is_fitted:  
+            raise RuntimeError("Model isn't fitted.")
 
-        n, d = X.shape
-        if d != self.n_input_dims_:
-            print('Incorrect dimensions for input data.')
-            sys.exit(0)
+        n, d_gmm = X_gmm.shape
+        _, d_reg = X_reg.shape
+        if d_gmm != self.n_gmm_dims_:
+            raise ValueError('Incorrect dimensions for the GMM input data.')
+        if d_reg != self.n_reg_dims_:
+            raise ValueError('Incorrect dimensions for the regression input data.')
         
         # Make sure we can iterate when n_tasks = 1
         if self.n_tasks_ == 1:
@@ -204,11 +208,11 @@ class GMMRegressor(object):
             reg_weights = self.reg_weights_
 
         # Determine test sample posterior probability
-        resp_tst = self.gmm_.predict_proba(X)
+        resp_tst = self.gmm_.predict_proba(X_gmm)
         labels_tst = np.argmax(resp_tst, axis=1)
 
         # Predict test targets
-        X_ext = np.concatenate((np.ones((n, 1)), X), axis=1)
+        X_ext = np.concatenate((np.ones((n, 1)), X_reg), axis=1)
         targets = np.zeros((n, self.n_tasks_))
         for k in range(self.n_components):
             dot_prod = np.dot(X_ext, reg_weights[:, :, k].T)            
@@ -219,7 +223,7 @@ class GMMRegressor(object):
 
         return targets
     
-    def score(self, X, y):
-        targets = self.predict(X)
+    def score(self, X_gmm, X_reg, y):
+        targets = self.predict(X_gmm, X_reg)
         score = r2_score(y, targets)
         return  score
--- cwlm/kmeans_regressor_mixed.py
+++ cwlm/kmeans_regressor_mixed.py
@@ -21,90 +21,94 @@ class KMeansRegressor(object):
         for k in range(self.n_components_):
             self.regs_.append(Ridge(alpha=self.alpha_))
     
-    def _check_data(self, X, y):
+    def _check_data(self, X_km, X_reg, y):
         """Check that the input data is correctly formatted.
 
         Parameters
         ----------
-        X : array-like, shape (n_samples, n_features)
-
+        X_km : array-like, shape (n_samples, n_km_features)
+        X_reg : array-like, shape (n_samples, n_reg_features)
         y : array, shape (n_samples, n_targets)
 
         Returns
         -------
-        t : int
-            The total number of targets.
-        
-        n : int
-            The total number of samples.
-
-        d : int
-            The total number of features (dimensions)
-
+        t : int, the total number of targets
+        n : int, the total number of samples
+        d_km : int, the total number of GMM features
+        d_reg : int, the total number of regression features
         """
 
         if y.ndim == 1:
             y = y[:, np.newaxis]
 
-        n_x, d = X.shape
+        n_x_km, d_km = X_km.shape
+        n_x_reg, d_reg = X_reg.shape
         n_y, t = y.shape
+
+        if n_x_km == n_x_km:
+            n_x = n_x_km
+        else:
+            error_report = 'Both views of the input data must have 1 to 1 sample correspondence.'
+            error_details = '\nX_km has {} samples while X_reg has {} samples.'.format(n_x_km, n_x_reg) 
+            raise ValueError(error_report, error_details)
 
         if n_x == n_y:
             n = n_x
         else:
-            print('Data size error. Number of samples in X and y must match:')
-            print('X n_samples = %d, y n_samples = %d'%(n_x, n_y))
-            print('Exiting.')
-            sys.exit()
+            error_report = 'Data size error. Number of samples in X and y must match:'
+            error_details = 'X n_samples = {}, y n_samples = {}'.format(n_x, n_y)
+            raise ValueError(error_report, error_details)
 
-        return t, n, d, X, y
+        return t, n, d_km, d_reg, X_km, X_reg, y
 
 
-    def fit(self, X_tr, y_tr):
+    def fit(self, X_km, X_reg, y):
         """TODO: 
                 - Add docstring.
                 - Complete multioutput.
         """
         self.is_fitted_ = False
-        t, n, d, X_tr, y_tr = self._check_data(X_tr, y_tr)
-        labels_tr = self.kmeans_.fit_predict(X_tr)
-        reg_weights = np.empty((t, d+1, self.n_components_))
+        t, n, d_km, d_reg, X_km, X_reg, y = self._check_data(X_km, X_reg, y)
+        labels_tr = self.kmeans_.fit_predict(X_km)
+        reg_weights = np.empty((t, d_reg + 1, self.n_components_))
         reg_precisions = np.zeros((t, self.n_components_))
 
         for k in range(self.n_components_):
             idx = (labels_tr == k)
-            self.regs_[k].fit(X_tr[idx, :], y_tr[idx, :])
+            self.regs_[k].fit(X_reg[idx, :], y[idx, :])
             reg_weights[:, 0, k] = self.regs_[k].intercept_
             reg_weights[:, 1:, k] = self.regs_[k].coef_
-            reg_vars = np.var(y_tr[idx, :], axis=0)
+            reg_vars = np.var(y[idx, :], axis=0)
             eps = 10 * np.finfo(reg_vars.dtype).eps
             reg_precisions[:, k] = 1/(reg_vars + eps)
 
         self.n_tasks_ = t
-        self.n_input_dims_ = d
+        self.n_km_dims_ = d_km
+        self.n_reg_dims_ = d_reg
         self.labels_tr_ = labels_tr
         self.reg_weights_ = reg_weights.squeeze()
         self.reg_precisions_ = reg_precisions.squeeze()
         self.is_fitted = True
         self.cluster_centers_ = self.kmeans_.cluster_centers_
     
-    def predict(self, X_tst):
-        if not self.is_fitted: 
-            print("Model isn't fitted.")
-            return
+    def predict(self, X_km, X_reg):
+        if not self.is_fitted:  
+            raise RuntimeError("Model isn't fitted.")
 
-        n, d = X_tst.shape
-        if d != self.n_input_dims_:
-            print('Incorrect dimensions for input data.')
-            sys.exit(0)
+        n, d_km = X_km.shape
+        _, d_reg = X_reg.shape
+        if d_km != self.n_km_dims_:
+            raise ValueError('Incorrect dimensions for the KM input data.')
+        if d_reg != self.n_reg_dims_:
+            raise ValueError('Incorrect dimensions for the regression input data.')
 
-        labels_tst = self.kmeans_.predict(X_tst)
+        labels_tst = self.kmeans_.predict(X_km)
         targets = np.empty((n, self.n_tasks_))
         for k in range(self.n_components_):
             idx = (labels_tst == k)
             # Check for empty clusters
             if sum(idx) != 0:
-                targets[idx, :] = self.regs_[k].predict(X_tst[idx, :])
+                targets[idx, :] = self.regs_[k].predict(X[idx, :])
             else:
                 if self.verbose:
                     print("No test samples in cluster %d"%k)
@@ -114,9 +118,9 @@ class KMeansRegressor(object):
         
         return targets
     
-    def score(self, X_tst, y_tst):
+    def score(self, X_km, X_reg, y):
         '''TODO: this needs extra metrics.
         '''
-        targets = self.predict(X_tst)
-        score = r2_score(y_tst, targets)
+        targets = self.predict(X_km, X_reg)
+        score = r2_score(y, targets)
         return  score
